05fda4e302f3edf13b60974db5ead9e4e09daf85
Sergey Moryakov <sergey@moryakov.com>
2023-08-08 15:48:31 +0300

    setting checkout - left panel

---
+++ src/js/data.js
@@ -0,0 +1,15 @@
+export const customersData = [
+    {
+        custId: "";
+        custLegalName: "AeroSupplyPlus LLC",
+        custAddress: "2023, Cactus Road, Springdale, FL, 33761 USA",
+        custBillToAddress: "2023, Cactus Road, Springdale, FL, 33761 USA",
+        custPhone: "+1 898 928928928",
+        custEmail: "supply@aerosupplyplus.aero",
+        custContactName: "Mattew Yampolski",
+        custKYChecked: "",
+        custPaymentTerms: "Net30",
+        custPaymentMethods: 
+        custStatus: "active",
+    },
+];
